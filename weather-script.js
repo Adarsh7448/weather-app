19fc7b099a3a80b1f1a43d828a0c65d9fa11ae52
AdarshMadre <adarsh.madre2@gmail.com>
2023-09-12 23:55:26 +0530

    integrated App with UI, components made

--- weather-script.js
+++ weather-script.js
@@ -1,26 +1,59 @@
+Vue.component('city-card',{
+    props:['city'],
+    data: function(){
+        return {
+            appid: 'bc9ee8be5d9f4f84f2180759fe53b134',
+            status:0,
+            image:'images/dummy.jpg',
+            maxTemp:'Not Available',
+            minTemp:'Not Available',
+            condition:'Unknown'
+            }
+    },
+    template: `<div class="card m-2" style="width: 19rem;">
+                    <img v-bind:src="image" class="card-img-top mt-2" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ city }}</h5>
+                        <p class="card-text mb-1">Max Temperature: {{maxTemp}}</p>
+                        <p class="card-text">Min Temperature: {{minTemp}}</p>
+                        <p class="card-text">{{condition}}</p>
+                        <a @click="getWeather" class="btn btn-warning">Update</a>
+                        <a @click="delCity(city)" class="btn btn-danger">Delete</a>
+                    </div>
+                </div>`,
+    methods:{
+        getWeather: async function(){
+                    let url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.appid}`
+                    let response = await fetch(url);
+                    let output = await response.json();
+                    this.status = output.cod;
+                    this.minTemp = math.round(output.main.temp_min - 273,2);
+                    this.maxTemp = math.round(output.main.temp_max - 273,2);
+                    // console.log(output)            
+                },
+        delCity: function(city){
+            this.$emit('delcity', city);
+        }
+        
+    }
+})
+
 var app = new Vue({
     el:'#myapp',
     data:{
         city:'',
-        appid: 'bc9ee8be5d9f4f84f2180759fe53b134',
-        minTemp:0,
-        maxTemp:0,
-        status:0
+        addedCities:[]
     },
     methods:{
-        getWeather: async function(){
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.appid}`
-            let response = await fetch(url);
-            let output = await response.json();
-            this.status = output.cod;
-            this.minTemp = math.round(output.main.temp_min - 273,2);
-            this.maxTemp = math.round(output.main.temp_max - 273,2);
-            // console.log(output)            
+        addCity: function(){
+            this.addedCities.push(this.city);
+            this.city = '';
+        },
+        delCity: function(citytoDel){
+            console.log('delete caught', citytoDel)
+            this.addedCities = this.addedCities.filter(function (city){
+                return city !== citytoDel;
+            });
         }
     },
-    computed:{
-        avgTemp: function(){
-            return (this.maxTemp + this.minTemp)/2;
-        }
-    }
 })
